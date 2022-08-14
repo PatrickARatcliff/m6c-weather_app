73755deb8f0d4b83fe653d0785a418547da10a23
PatrickARatcliff <pratcliff5@gmail.com>
2022-08-14 13:37:43 -0600

    js: add const selectors for page elements

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+//select elements for current city data
+const cityDateEl = document.querySelector('#cityDate');
+const cityTempEl = document.querySelector('#cityTemp');
+const cityWindEl = document.querySelector('#cityWind');
+const cityHumid = document.querySelector('#cityHumid');
+const cityUvEl = document.querySelector('#cityUv');
+//select elements for 5-day forecast cards
+const day1El = document.querySelector('#day1');
+const day2El = document.querySelector('#day2');
+const day3El = document.querySelector('#day3');
+const day4El = document.querySelector('#day4');
+const day5El = document.querySelector('#day5');
+//select input, search button, andsearch history element for saved cities
+const cityHistEl = document.querySelector('#cityHist');
+const searchInputEl = document.querySelector('#searchBtn');
+const searchBtnEl= document.querySelector('#cityHist');
