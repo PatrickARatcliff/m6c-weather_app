100358e1ac7f122ccd2ffdbfa6488f69a775e7e7
PatrickARatcliff <pratcliff5@gmail.com>
2022-08-14 14:44:25 -0600

    js: writeData function, assign variables, apply text content to html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,11 +14,14 @@ const day5El = document.querySelector('#day5');
 const cityHistEl = document.querySelector('#cityHist');
 const searchInputEl = document.querySelector('#searchBtn');
 const searchBtnEl= document.querySelector('#cityHist');
-//create const for fetch url
-const apiKey = ''; //api key
-const searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`; //api URL as template literal
+
 //fetch data request
+let searchCity = '';
+//let dataResp;
 const fetchData = function() {
+    //create const for fetch url
+    const apiKey = ''; //api key
+    const searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`; //api URL as template literal
     //send api request
     fetch(searchUrl)
     //receive response in json
@@ -27,8 +30,20 @@ const fetchData = function() {
     .then((data) => console.log(data));
 };
 //use data object to populate html elements
-const writeData = function () {
-    
+const writeData = function(data) {
+    //assign data to variables
+    const cityName = fetchData.data.name;
+    const cityTemp = fetchData.data.main.temp;
+    const cityWind = fetchData.data.wind.speed;
+    const cityHumid = fetchData.data.main.humidity;
+    const cityIcon = fetchData.data.weather.icon
+    //const cityUv = data.
+    //change text of html elements
+    cityDateEl.textContent() = `${cityName} (##/##/####) ${cityIcon}`
+    cityTempEl.textContent() = `Temp: ${cityTemp}`;
+    cityWindEl.textContent() = `Wind: ${cityWind}`;
+    cityTempEl.textContent() = `Humidity: ${cityHumid}`;
+
 };
 
 
